ab7762f75d92b74cf111bc861b1ba14c90bda06e
UtkrishtPathak <utkrishtpathak@gmail.com>
2021-05-31 11:44:51 +0530

    error fix

--- src/jobs/pin.js
+++ src/jobs/pin.js
@@ -9,7 +9,7 @@ module.exports =
         
         //to get the current date in the format dd-mm-yyyy
         var today = new Date();
-        var dd = String(today.getDate()+1).padStart(2, '0');
+        var dd = String(today.getDate()).padStart(2, '0');
         var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
         var yyyy = today.getFullYear();
         today = dd+"-"+mm+"-"+yyyy;
