b512d3156b9beadf1a874688fee2541f0cbb6e4d
UtkrishtPathak <utkrishtpathak@gmail.com>
2021-06-02 19:22:11 +0530

    aaaaaah

--- src/jobs/slots.js
+++ src/jobs/slots.js
@@ -32,9 +32,8 @@ module.exports =
                 { headers: { 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36 Edg/90.0.818.51'}})
                 .then(function (response) //response recieved from the "GET" request sent
                     {
-                        console.log(response);  //storing the centres data
+                        centres=response.data.data.sessions;  //storing the centres data
                         var n=0;  //to check whether no centres have any available capacity
-
                         //A forEach loop for getting individual centres from the given list of centres
                         centres.forEach((centre) => 
                         {
